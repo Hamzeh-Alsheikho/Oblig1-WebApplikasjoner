a180cab629a32ad4eb49e6379dd9dfdf8e21aef6
Hamzeh0086 <s339572@oslomet.no>
2021-09-03 10:00:25 +0200

    Add project files.

---
+++ Oblig1/Controllers/KundeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication24.Models;
+
+namespace WebApplication24.Controllers
+{
+    [Route("[Controller]/[action]")]
+    public class KundeController : ControllerBase
+    {
+        public List<Kunde> HentAlle()
+        {
+            var kundene = new List<Kunde>();
+
+            var kunde1 = new Kunde();
+            kunde1.navn = "Per Hansen";
+            kunde1.adresse = "Osloveien 82";
+
+            var kunde2 = new Kunde
+            {
+                navn = "Line Jensen",
+                adresse = "Askerveien 82"
+            };
+
+            kundene.Add(kunde1);
+            kundene.Add(kunde2);
+
+            return kundene;
+        }
+    }
+}
---
+++ Oblig1/Models/Kunde.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication24.Models
+{
+    public class Kunde
+    {
+        public String navn { get; set; }
+
+        public string adresse { get; set; }
+    }
+}
