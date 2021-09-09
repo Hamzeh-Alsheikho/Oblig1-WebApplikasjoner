613fd840c33e492a351d011eadec01f79e0952d9
Hamzeh0086 <s339572@oslomet.no>
2021-09-09 14:01:59 +0200

    Adding endre function html and js

--- Oblig1/wwwroot/js/endre.js
+++ Oblig1/wwwroot/js/endre.js
@@ -1,1 +1,51 @@
-﻿
+﻿$(function () {
+    // hent kunden med kunde-id fra url og vis denne i skjemaet
+
+    const id = window.location.search.substring(1);
+    const url = "Kunde/HentEn?" + id;
+    $.get(url, function (kunde) {
+        id: $("#id").val(), // må ha med denne som ikke har blitt endret for å vite hvilken kunde som skal endres
+        fornavn: $("#fornavn").val(),
+        etternavn: $("#etternavn").val(),
+        adresse: $("#adresse").val(),
+        postnr: $("#postnr").val(),
+        poststed: $("#poststed").val(),
+        destination: $("#reiseMal").val(),
+        antallAdult: $("#antallAdult").val(),
+        antallChild: $("#antallChild").val(),
+        ticketType: getTicketType(),
+        departureDato: $("#avgang").val(),
+        returnDato: $("#retur").val(),
+        ticketClass: getKlassetType(),
+        telfonnr: $("#telefonner").val(),
+        epost: $("#epost").val()
+    });
+});
+
+function endreKunde() {
+    const kunde = {
+        id: $("#id").val(), // må ha med denne som ikke har blitt endret for å vite hvilken kunde som skal endres
+        fornavn: $("#fornavn").val(),
+        etternavn: $("#etternavn").val(),
+        adresse: $("#adresse").val(),
+        postnr: $("#postnr").val(),
+        poststed: $("#poststed").val(),
+        destination: $("#reiseMal").val(),
+        antallAdult: $("#antallAdult").val(),
+        antallChild: $("#antallChild").val(),
+        ticketType: getTicketType(),
+        departureDato: $("#avgang").val(),
+        returnDato: $("#retur").val(),
+        ticketClass: getKlassetType(),
+        telfonnr: $("#telefonner").val(),
+        epost: $("#epost").val()
+    };
+    $.post("Kunde/Endre", kunde, function (OK) {
+        if (OK) {
+            window.location.href = 'index.html';
+        }
+        else {
+            $("#feil").html("Feil i db - prøv igjen senere");
+        }
+    });
+}
